1c83a0baafc618f83d3e7567052dfad9d9dc43e5
idkbutiampoggers <119977629+idkbutiampoggers@users.noreply.github.com>
2023-04-22 21:50:50 +1200

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,6 @@
 -- Made by JohnyGaming#2917, make sure to credit
+-- also credits to Master Oogway#3595
+
 if _G.ThirdPerson then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/idkbutiampoggers/JeffTheKillerMorphV2/main/ThirdPerson.lua"))()
 else
